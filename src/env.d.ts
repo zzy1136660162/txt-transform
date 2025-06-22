fc80bae38c74bc8c8975528e6a75977de436c254
张子扬 <1136660162@qq.com>
2025-06-22 21:41:09 +0800

    msg

--- src/env.d.ts
+++ src/env.d.ts
@@ -23,3 +23,8 @@ declare global {
     myAPI: MyAPI;
   }
 }
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<Record<string, unknown>, Record<string, unknown>, any>
+  export default component
+}
